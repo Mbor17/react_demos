c2c2daa95ac02e4ff60e05accdb6382dadce1792
Mbor17 <borovetc@gmail.com>
2024-05-06 01:55:58 +0300

    part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React, {FC} from 'react';
 import './App.css';
+import Character from "./components/character/Character";
 
- const App:FC =()=> {
-  return (
-    <>
+const App: FC = () => {
+    return (
+        <>
+            <Character
+                name={'bart'}
+                image={"https://www.mubis.es/media/users/9164/92322/desmienten-la-muerte-de-bart-simpson-original.jpg"}
 
-    </>
-  );
+            />
+            <Character
+                name={'homer'}
+                image={"https://www.mubis.es/media/users/9164/92322/desmienten-la-muerte-de-bart-simpson-original.jpg"}
+            />
+            <Character
+                name={'marge'}
+                image={"https://i.pinimg.com/474x/85/f4/cf/85f4cfd3c777989e2858b561d991d137.jpg"}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad alias architecto, consectetur cumque
+                dicta maiores minima, optio quos repudiandae soluta tempora. Aut illo ipsa iure labore mollitia quaerat,
+                unde.
+            </Character>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/character/Character.tsx
@@ -0,0 +1,23 @@
+import React, {FC, ReactNode} from 'react';
+import styles from './Character.module.css';
+interface IProps{
+    name:string;
+    image:string;
+    // children?:React.ReactNode
+}
+type PropsWithChildren<T>=T& { children?: ReactNode };
+const Character :FC<PropsWithChildren <IProps> >= ({name,image,children}) => {
+      return (
+          <div>
+              <h2>{name}</h2>
+              <img
+                  className={styles.w200}
+                  src={image}
+                  alt={name}/>
+              <p>{children}</p>
+          </div>
+
+    );
+};
+
+export default Character;
