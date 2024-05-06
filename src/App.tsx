8a009030070df031c49fa5567e3b2216dbf0aa1c
Mbor17 <borovetc@gmail.com>
2024-05-06 19:26:12 +0300

    lesson1 components+fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, {FC} from 'react';
 import './App.css';
+import Character from "./components/character/Character";
+import Characters from "./components/characters/Characters";
+import Recipes from "./components/recipes/Recipes";
 
- const App:FC =()=> {
-  return (
-    <>
-
-    </>
-  );
+const App: FC = () => {
+    return (
+        <>
+            {/*<Characters/>*/}
+            <Recipes/>
+        </>
+    );
 }
 
 export default App;
