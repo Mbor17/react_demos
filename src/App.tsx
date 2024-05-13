c99e8f8cb1756f19d97e69255980ebe8742c48a8
Mbor17 <borovetc@gmail.com>
2024-05-14 00:59:03 +0300

    lesson2 axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
+import PostsComponent from "./components/posts/PostsComponent";
+import CommentsComponent from "./components/components/CommentsComponent";
+import {getAllCommentsOfSinglePost} from "./services/jpg.api.service";
+import {ICommentModel} from "./models/ICommentModel";
 
  const App:FC =()=> {
+     const [comments,setcomments] = useState<ICommentModel[]>([]);
+     const lift=(postId:number) =>{
+         getAllCommentsOfSinglePost(postId).then(({data}) => {
+            setcomments(data)
+         })
+     }
   return (
-    <>
-
-    </>
+    <div className="App">
+        <div className={'posts'}><PostsComponent lift={lift}/></div>
+        <div className={'comments'}><CommentsComponent comments={comments}/></div>
+    </div>
   );
 }
 
